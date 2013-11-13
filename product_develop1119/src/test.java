62ad4a368b73ee07fe232dcf378a46d793ab2edf
sanjusingh <naik.anokhi01@gmail.com>
2013-11-13 11:07:41 +0530

    the first part implementation

---
+++ product_develop1119/src/test.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class test {
+	student st;
+	@Before
+	public void setUp() throws Exception {
+		 st.setName("Rajeev");
+	      st.setAge(25);
+	      st.setCourse("mca");
+	}
+	
+
+	@Test
+	public void test() {
+		assertEquals
+		
+		fail("Not yet implemented");
+	}
+
+}

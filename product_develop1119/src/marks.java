c5ea7e10a559048cc34e219aaad35f68f9e32ff4
anokhi naik <naik.anokhi01@gmail.com>
2013-11-13 11:48:29 +0530

    test case2 implementated to check if the values are true, false, string=notnull

---
+++ product_develop1119/src/marks.java
@@ -0,0 +1,31 @@
+
+public class marks {
+	 private String name;
+	   private String course;
+	   private int SE;
+	   private int AI;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCourse() {
+		return course;
+	}
+	public void setCourse(String course) {
+		this.course = course;
+	}
+	public int getSE() {
+		return SE;
+	}
+	public void setSE(int sE) {
+		SE = sE;
+	}
+	public int getAI() {
+		return AI;
+	}
+	public void setAI(int aI) {
+		AI = aI;
+	}
+}
--- product_develop1119/src/test.java
+++ product_develop1119/src/test.java
@@ -6,10 +6,11 @@ import org.junit.Test;
 
 public class test {
 	student st;
+	marks mk;
 	@Before
 	public void setUp() throws Exception {
 		st = new student();
-		
+		mk = new marks();
 	}
 	
 
@@ -23,4 +24,24 @@ public class test {
 		
 	}
 
+	public void testmarks() {
+		String Name = new String ("anokhi");
+	      String course = new String ("mca");
+	   
+	      mk.setSE(50);
+	      mk.setAI(40);
+	      //Check that a condition is true
+	      assertTrue(50==mk.getAI());
+
+	      //Check that a condition is false
+	      assertFalse(30==mk.getSE());
+
+	      //Check that an object isn't null
+	      assertNotNull(Name);
+	      assertNotNull(course);
+
+	      
+		
+	}
+	
 }
